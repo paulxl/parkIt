49378bb44c69be88fe2807e9fefa74d67a33bd0e
paul <pxl34232@gmail.com>
2020-12-14 12:32:14 -0500

    problem shooting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const server = http.Server(app);
-app.use(express.static("Public/welcome_Page"));
+app.use(express.static("Public/welcome_Page/index.html"));
 
 server.listen(PORT, () =>
   console.log(`Server running on flamingo juice and port ${PORT}   🔥`)
