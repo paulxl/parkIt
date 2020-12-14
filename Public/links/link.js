d5255d0c98d2b54ed8f81bfc95e131846928d378
Bradley Pratt <bradapratt@gmail.com>
2020-12-14 10:59:14 -0800

    added some functionality to navbar

--- Public/links/link.js
+++ Public/links/link.js
@@ -1,3 +1,15 @@
+/*  Swaps out the words "It" and "You" in the logo when clicked */
+var button = document.getElementById("it_you");
+button.addEventListener('click', function() {
+  if (button.getAttribute("data-text-swap") == button.innerHTML) {
+    button.innerHTML = button.getAttribute("data-text-original");
+  } else {
+    button.setAttribute("data-text-original", button.innerHTML);
+    button.innerHTML = button.getAttribute("data-text-swap");
+  }
+}, false);
+
+/* Transforms the hamburg menu icon when clicked */
 function transformIcon(x) {
     x.classList.toggle("change");
 }
